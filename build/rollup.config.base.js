3258414130ef4e45626f24a1e565d71c9e835646
toxic-johann <353904974@qq.com>
2018-10-07 17:20:40 +0800

    build: 0.2.3
    upgrade babel and fix build system bug

--- build/rollup.config.base.js
+++ build/rollup.config.base.js
@@ -10,58 +10,58 @@ export const banner = `
  * ${name} v${version}
  * (c) 2017-${(new Date()).getFullYear()} ${author}
  * Released under ${license}
+ * Built ad ${new Date()}
  */
 `;
 import babel from 'rollup-plugin-babel';
-import typescript from 'rollup-plugin-typescript';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 const babelConfig = {
   common: {
     presets: [
-      [ 'env', {
+      [ '@babel/preset-env', {
         modules: false,
         targets: {
           browsers: [ 'last 2 versions', 'not ie <= 8' ],
         },
       }],
     ],
-    plugins: [ 'transform-runtime', 'lodash' ],
+    plugins: [ '@babel/plugin-transform-runtime', 'lodash' ],
     exclude: 'node_modules/**',
     runtimeHelpers: true,
     babelrc: false,
   },
   es: {
     presets: [
-      [ 'env', {
+      [ '@babel/preset-env', {
         modules: false,
         targets: {
           browsers: [ 'last 2 versions', 'not ie <= 8' ],
         },
       }],
     ],
-    plugins: [ 'transform-runtime', 'lodash' ],
+    plugins: [ '@babel/plugin-transform-runtime', 'lodash' ],
     exclude: 'node_modules/**',
     runtimeHelpers: true,
     babelrc: false,
   },
   umd: {
     presets: [
-      [ 'env', {
+      [ '@babel/preset-env', {
         modules: false,
         targets: {
           browsers: [ 'last 2 versions', 'not ie <= 8' ],
         },
       }],
     ],
-    plugins: [ 'transform-runtime', 'lodash' ],
+    plugins: [ '@babel/plugin-transform-runtime', 'lodash' ],
     exclude: 'node_modules/**',
     runtimeHelpers: true,
     babelrc: false,
   },
   iife: {
     presets: [
-      [ 'env', {
+      [ '@babel/preset-env', {
         modules: false,
         targets: {
           browsers: [ 'last 2 versions', 'not ie <= 8' ],
@@ -74,7 +74,7 @@ const babelConfig = {
   },
   min: {
     presets: [
-      [ 'env', {
+      [ '@babel/preset-env', {
         modules: false,
         targets: {
           browsers: [ 'last 2 versions', 'not ie <= 8' ],
@@ -89,19 +89,22 @@ const babelConfig = {
 const externalRegExp = new RegExp(Object.keys(dependencies).join('|'));
 export default function(mode) {
   return {
-    input: 'src/index.ts',
+    input: 'ts-out/index.js',
     external(id) {
       return !/min|umd|iife/.test(mode) && externalRegExp.test(id);
     },
     plugins: [
-      typescript(),
       babel(babelConfig[mode]),
       commonjs({
         namedExports: {
-          lodash: 'node_modules/lodash/index.js',
+          lodash: [ 'node_modules/lodash/index.js' ],
         },
       }),
-      resolve(),
+      resolve({
+        customResolveOptions: {
+          moduleDirectory: [ 'ts-out', 'node_modules' ],
+        },
+      }),
     ],
   };
 }
